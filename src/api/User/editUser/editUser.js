d8a7b1d75e2aaba9e8260a6515fc1b70323d7e46
YunHee Lee <yunheenet@gmail.com>
2020-02-19 12:20:59 +0900

    MOD SeeUser API parameters

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -2,8 +2,8 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    editUser: async (_, args, { request, isAthenticated }) => {
-      isAthenticated(request);
+    editUser: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
       const { username, firstName, lastName, bio, avatar } = args;
       const { user } = request;
 
