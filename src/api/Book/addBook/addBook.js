693a96758415bfb53ecccce5240f04221d0e8b40
YunHee Lee <yunheenet@gmail.com>
2020-02-21 10:04:19 +0900

    MOD Add Book

--- src/api/Book/addBook/addBook.js
+++ src/api/Book/addBook/addBook.js
@@ -1,34 +1,52 @@
 import { isAuthenticated } from "../../../middlewares";
 import { prisma } from "../../../../generated/prisma-client";
+import { requestInterparkBookSearch } from "../../../utils";
 
 export default {
   Mutation: {
     addBook: async (_, args, { request }) => {
       isAuthenticated(request);
-      const {
-        isbn,
-        title,
-        author,
-        publisher,
-        coverLargeUrl,
-        coverSmallUrl,
-        link,
-        mobileLink
-      } = args;
+      const { isbn } = args;
       const { user } = request;
 
-      const book = await prisma.createBook({
-        isbn,
-        title,
-        author,
-        publisher,
-        coverLargeUrl,
-        coverSmallUrl,
-        link,
-        mobileLink
-      });
+      try {
+        const findBook = await prisma.books({
+          where: {
+            isbn
+          }
+        });
 
-      return book;
+        if (findBook.length !== 0) {
+          return findBook[0];
+        }
+
+        const {
+          title,
+          author,
+          publisher,
+          description,
+          coverLargeUrl,
+          coverSmallUrl,
+          link,
+          mobileLink
+        } = await requestInterparkBookSearch(isbn);
+
+        const book = await prisma.createBook({
+          isbn,
+          title,
+          author,
+          description,
+          publisher,
+          coverLargeUrl,
+          coverSmallUrl,
+          link,
+          mobileLink
+        });
+        console.log("createBook:" + book.isbn);
+        return book;
+      } catch (e) {
+        throw Error(e);
+      }
     }
   }
 };
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sendGridTransport from "nodemailer-sendgrid-transport";
 import jwt from "jsonwebtoken";
+import axios from "axios";
+import "./env";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -23,9 +25,29 @@ export const sendSecretMail = (address, secret) => {
     from: "ggobooking@gmail.com",
     to: address,
     subject: "🔐Login Secret for GgoBook🔐",
-    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/Website to log in`
+    html: `Hello! Your login secret is <strong>${secret}</strong><br/>Copy paste on the app/Website to log in`
   };
   return sendMail(email);
 };
 
 export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
+
+export const requestInterparkBookSearch = async isbn => {
+  const INTERPARK_KEY = process.env.INTERPARK_KEY;
+  const query = `http://book.interpark.com/api/search.api?key=${INTERPARK_KEY}&query=${isbn}&queryType=isbn&output=json`;
+
+  try {
+    const {
+      data: { item }
+    } = await axios.get(query);
+
+    if (item.length == 0) {
+      throw Error("Can't find this book.");
+    }
+
+    return item[0];
+  } catch (e) {
+    console.log(e);
+    throw Error("Request fail.");
+  }
+};
