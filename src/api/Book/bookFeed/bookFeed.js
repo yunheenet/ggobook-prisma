a07924d8510a47c96d05c8df17fd8f893d8f7483
YunHee Lee <yunheenet@gmail.com>
2020-02-27 09:56:37 +0900

    ADD Feed(Book)

---
+++ src/api/Book/bookFeed/bookFeed.js
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    bookFeed: async (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const following = await prisma.user({ id: user.id }).following();
+
+      return prisma.books({
+        where: {
+          user: {
+            id_in: [...following.map(user => user.id), user.id]
+          }
+        },
+        orderBy: "createdAt_DESC"
+      });
+    }
+  }
+};
