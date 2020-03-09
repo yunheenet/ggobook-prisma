88a623ad191b106a87dd495628acb1dfe55581d2
YunHee Lee <yunheenet@gmail.com>
2020-03-09 14:49:14 +0900

    MOD 회원가입시 꼬북 팔로윙 및 팔로워로 추가

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,19 +4,33 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+
       const exists = await prisma.$exists.user({
         OR: [{ username }, { email }]
       });
+
       if (exists) {
         throw Error("This username / email is already used it.");
       }
+
       await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
-        bio
+        bio,
+        following: {
+          connect: {
+            email: "ggobooking@gmail.com"
+          }
+        },
+        followers: {
+          connect: {
+            email: "ggobooking@gmail.com"
+          }
+        }
       });
+
       return true;
     }
   }
